0543b007a26901b92197c3213478c8f6e7b7888b
Emerson Franciscsco <emersonairton@hotmail.com>
2022-05-27 23:07:59 -0300

    formatation code

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,18 +12,18 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		try {
-		System.out.println("Enter account data");
-		System.out.print("Number: ");
-		int number = sc.nextInt();
-		System.out.print("Holder: ");
-		sc.nextLine();
-		String name = sc.nextLine();
-		System.out.print("Initial balance: ");
-		double balance = sc.nextDouble();
-		System.out.print("Withdraw limit: : ");
-		double limit = sc.nextDouble();
-		Account acc = new Account(number, name, balance, limit);
-		
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: : ");
+			double limit = sc.nextDouble();
+			Account acc = new Account(number, name, balance, limit);
+
 			System.out.print("\nEnter amount for withdraw: ");
 			acc.withdraw(sc.nextDouble());
 			System.out.printf("New balance: %.2f", acc.getBalance());
